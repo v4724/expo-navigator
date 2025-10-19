8272ca45c9a50b95c89baa60733e84f33b3ea0dd
v4724 <v47244724@gmail.com>
2025-10-19 18:14:19 +0800

    core: 設定 mat-font 樣式 import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import {
   ApplicationConfig,
+  inject,
   provideBrowserGlobalErrorListeners,
+  provideEnvironmentInitializer,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
@@ -11,6 +13,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideHttpClient } from '@angular/common/http';
+import { MatIconRegistry } from '@angular/material/icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,5 +28,16 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideEnvironmentInitializer(() => {
+      const matIconRegistry = inject(MatIconRegistry);
+      // 設定預設字體集
+      matIconRegistry.setDefaultFontSetClass(
+        'material-symbols',
+        'material-symbols-rounded',
+        'material-symbols-sharp',
+        'material-symbols-outlined',
+        'mat-ligature-font',
+      );
+    }),
   ],
 };
