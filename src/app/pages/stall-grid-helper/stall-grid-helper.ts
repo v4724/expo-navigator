1afc76c55292d8e859e9137229dadce19ebe01a4
v4724 <v47244724@gmail.com>
2025-10-21 19:08:21 +0800

    chore: 移除沒用到的 cmp, 調整 user info popover 大小

--- src/app/pages/stall-grid-helper/stall-grid-helper.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-stall-grid-helper',
-  imports: [],
-  templateUrl: './stall-grid-helper.html',
-  styleUrl: './stall-grid-helper.scss',
-})
-export class StallGridHelper {}
