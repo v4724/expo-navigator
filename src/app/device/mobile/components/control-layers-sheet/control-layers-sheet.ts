661fc2c79ef0228a6529ad10d896209632b4bcb4
v4724 <v47244724@gmail.com>
2025-11-16 22:48:37 +0800

    feature: 加上 mobile 圖層內容

--- src/app/device/mobile/components/control-layers-sheet/control-layers-sheet.ts
+++ src/app/device/mobile/components/control-layers-sheet/control-layers-sheet.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import {
   IonModal,
   IonContent,
-  IonList,
-  IonAvatar,
-  IonLabel,
-  IonImg,
-  IonItem,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
 } from '@ionic/angular/standalone';
+import { OnlyArea } from 'src/app/components/layers-controller/only-area/only-area';
+import { StallsLayer } from 'src/app/components/layers-controller/stalls-layer/stalls-layer';
+import { MarkedStallService } from 'src/app/core/services/state/marked-stall-service';
 
 @Component({
   selector: 'app-control-layers-sheet',
-  imports: [IonModal, IonContent, IonList, IonAvatar, IonLabel, IonImg, IonItem],
+  imports: [
+    IonModal,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonButton,
+    StallsLayer,
+    OnlyArea,
+  ],
   templateUrl: './control-layers-sheet.html',
   styleUrl: './control-layers-sheet.scss',
 })
-export class ControlLayersSheet {}
+export class ControlLayersSheet {
+  private _markedListService = inject(MarkedStallService);
+
+  show = toSignal(this._markedListService.layerShown$);
+  toggleLayer() {
+    this._markedListService.toggleLayer();
+  }
+}
