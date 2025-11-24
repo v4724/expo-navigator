e04433f8464944184ebf5a64951284f2194d1498
v4724 <v47244724@gmail.com>
2025-11-24 18:48:11 +0800

    feature: mobile 書籤加上 toggle 開關

--- src/app/device/mobile/components/marked-list-drawer/marked-list-drawer.ts
+++ src/app/device/mobile/components/marked-list-drawer/marked-list-drawer.ts
@@ -1,18 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { BookmarkList } from 'src/app/shared/components/marked-list/bookmark-list/bookmark-list';
 import { CreateMarkedListBtn } from 'src/app/shared/components/marked-list/create-marked-list-btn/create-marked-list-btn';
 import { DrawerOnMobile } from 'src/app/shared/components/drawer-on-mobile/drawer-on-mobile';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { FormsModule } from '@angular/forms';
+import { MarkedStallService } from 'src/app/core/services/state/marked-stall-service';
 
 @Component({
   selector: 'app-marked-list-drawer',
-  imports: [CommonModule, ButtonModule, CreateMarkedListBtn, BookmarkList, DrawerOnMobile],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    CreateMarkedListBtn,
+    BookmarkList,
+    DrawerOnMobile,
+    ToggleSwitch,
+    FormsModule,
+  ],
   templateUrl: './marked-list-drawer.html',
   styleUrl: './marked-list-drawer.scss',
 })
-export class MarkedListDrawer {
+export class MarkedListDrawer implements OnInit {
   @ViewChild(DrawerOnMobile) drawer!: DrawerOnMobile;
+
+  private _markedListService = inject(MarkedStallService);
+
+  checked = false;
+
+  ngOnInit(): void {
+    this._markedListService.layerShown$.subscribe((val) => {
+      this.checked = val;
+    });
+  }
 
   show() {
     this.drawer.show();
@@ -20,5 +41,9 @@ export class MarkedListDrawer {
 
   close() {
     this.drawer.close();
+  }
+
+  toggleLayer() {
+    this._markedListService.toggleLayer();
   }
 }
