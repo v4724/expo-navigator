afa44714fb81c23371c7c9095b8ec7744092b6b1
v4724 <v47244724@gmail.com>
2025-12-02 02:35:10 +0800

    chore: 更新定義檔來源為 同忍祭, 調整搜尋提示文字

--- src/app/core/const/google-excel-csv-url.ts
+++ src/app/core/const/google-excel-csv-url.ts
@@ -2,4 +2,4 @@
 // IMPORTANT: Replace this with your Google Sheet's "Publish to web" CSV URL.
 // In Google Sheets: File > Share > Publish to web > Select a sheet & "Comma-separated values (.csv)" > Publish.
 export const DEF_CSV_URL =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vS-6P2J3HQhJvEb5Cl8RnSrceSMUafFXrKwLPa8zqgB64nUDSv0n34qKM12Rc59lonN69n5a-h6SraF/pub?gid=450047636&single=true&output=csv';
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWJgpPiGYJzvL1fz8udSglIOm3Kx3YgobNXoYun6JMxJdu-sSCX_A2YCxDT0DIz9g48Ab5dVxXEdQf/pub?gid=672205595&single=true&output=csv';
